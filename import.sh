9caf8434d6d14c37550bd57bb5386bf42a5a153f
Mike <michael.andreae@vicu.utoronto.ca>
2019-11-18 14:18:22 -0500

    removed unused files

--- import.sh
+++ import.sh
@@ -3,7 +3,6 @@
 
 localDir="/home/maximus/xml"
 
-
 # Open STDOUT as a file for write.
 exec 1>$localDir/log/scrapeAtoM.txt
 
@@ -14,23 +13,10 @@ exec 2>$localDir/log/stderr.txt
 rm -R $localDir/scrape/*
 
 echo "######## UNZIPPIING FONDS FROM UPLOAD FOLDER ########"
-#IFS=$'\n'       # make newlines the only separator
-#set -f          # disable globbing
 
 tar -xf /home/upload/victoria-university-archives.tar.gz -C /home/maximus/xml/scrape/
 
 cp $localDir/scrape/victoria-university-archives/descRecs/* $localDir/scrape
-
-echo "########## FINISHED SCRAPING FONDS ##########"
-
-#this is a failsafe to ensure the right number of files are there after scrape before purging the db
-numFiles=$(find $localDir/scrape/ -type f | wc -l)
-numCollections=$(cat fonds.txt | wc -l)
-
-if [ "$numFiles" != "$numCollections" ]
-then
-   echo "######## THERE ARE NOT 173 COLLECTIONS SCRAPED ########"
-fi
 
 echo "####### PURGING LOCAL DATABASE ########"
 #The database has to be purged because it doesn't delete the slug and clear database so AtoM gets too many records
@@ -47,8 +33,6 @@ exec 1>$localDir/log/import.txt
 echo "######## IMPORTING #######"
 
 php /usr/share/nginx/atom/symfony import:bulk --update="delete-and-replace" /home/maximus/xml/scrape
-
-#mysql -u delete atom -e 'DELETE FROM term_i18n WHERE culture NOT LIKE "en";'
 
 echo "######## REBUILDING CACHE #######"
 #if you want the local AtoM instance to show the collection
